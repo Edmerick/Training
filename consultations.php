4440b6351cc5fb26b83db610e320caabddef0a4c
Brice <bricekouakou@gmail.com>
2020-03-05 22:43:13 +0000

    change directories and consultation format

--- consultations.php
+++ consultations.php
@@ -2,14 +2,31 @@
 /**
  * Display contact entry file
  */
+include_once "menus.html";
 $letterGroups = array();
 $oldContact = file_get_contents("contacts.json");
-$persons = json_decode($oldContact);
-print_r($persons);
-for ($i=0; $i < count($persons) ; $i++) { 
-    $person = $persons[$i];
-    echo '<li class="list-group-item">'.$person["nom"].'</li>';
+$people = json_decode($oldContact, true);
+for ($i=0; $i< count($people); $i++){
 
-   echo '</ul>';
+    $firstLetter = substr ($people[$i]["nom"], 0 , 1);
+    $letterGroups[$firstLetter][] = $people[$i]; 
 }
+// Sorting the letters
+ksort($letterGroups); // sort multidimensional array by top-level keys
+echo'<div class="row justify-content-center">';
+    echo'<div class="col-6">';
+    // Print results in HTML
+    foreach($letterGroups as $key => $persons){
+
+    // affiche la lettre en majuscle
+        echo '<li class="list-group-item active">' .ucfirst($key) .'</li>';
+                for ($i=0; $i < count($persons) ; $i++) { 
+                    $person = $persons[$i];
+                    echo '<li class="list-group-item">'.$person["nom"].' '.$person["prenom"].'</li>';
+                }
+            echo "<br>";
+
+    }
+    echo'<div>';
+echo'<div>';
 ?>
--- index.php
+++ index.php
@@ -1,26 +1,7 @@
-<!DOCTYPE Html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Insertion Contact</title>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-   <!--Begin of Menu Bar-->
-   <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link active" href="#">Accueil<span class="sr-only">(current)</span></a>
-        </li>
-        <li class="dropdown">  <a href="/brice-training/tp-45mn/index.php" class="nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Enregistrements <span class="caret"></span></a>
-        </li>
-        <li class="dropdown">  <a href="/brice-training/tp-45mn/consultations.php" class="nav-link" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Consultations<span class="caret"></span></a>
-        </li>    
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-    </ul>
-    <hr class="my-1"> 
-    <!--End of menu bar-->
+
+<?php
+include_once("menus.html");
+?>
 <div class="container">
 <form role="form" name="inscription" method="post" action="connexion.php">
     <hr class="my-2">
